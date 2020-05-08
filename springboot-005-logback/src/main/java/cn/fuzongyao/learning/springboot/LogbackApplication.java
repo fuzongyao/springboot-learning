93e49218c526e6fcc33a5ced9d545c7d24c0af8a
fuzongyao <fuzongyao.java@qq.com>
2020-05-08 18:18:55 +0800

    feature(springboot-004-scheduled): Springboot logback 日志配置

---
+++ springboot-005-logback/src/main/java/cn/fuzongyao/learning/springboot/LogbackApplication.java
@@ -0,0 +1,19 @@
+package cn.fuzongyao.learning.springboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fuzongyao
+ * @date 2020/05/08
+ * @since 1.0.0
+ */
+@Slf4j
+@SpringBootApplication
+public class LogbackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackApplication.class, args);
+        log.debug("debug");
+    }
+}
